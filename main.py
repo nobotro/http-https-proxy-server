a8fdbef858c8d9a01f990b7f777d7ce29bdfb002
Administrator <kevlishvilimisha@gmail.com>
2017-06-04 12:31:17 +0200

    proxy receiver server

--- main.py
+++ main.py
@@ -30,26 +30,63 @@ class server_manager():
         conn.settimeout(settings.socket_timeout)
         json_data=json.loads(conn.recv(4000).decode(),encoding='utf-8')
         conn.settimeout(None)
+        responce_fragments=[]
 
 
+        #ეს ბრძანება მოდის როცა კლიენტი გვიგზავნის ვებ რექუესთის ფრაგმენტს
+        #ჩვენ ვუგზავნით 'ack'-ს რომ დავადასტუროთ მიღება
 
-        if json_data['op']=='send':
+        if json_data['op']=='send_req_data':
 
             conn.settimeout(settings.socket_timeout)
             conn.sendall('ack')
             conn.settimeout(None)
 
-
+            #veb რექუესთების ლისტში,id-ის მიხედვით ვაგდებ ამ ფრაგმენტს
             if self.requests[json_data['req_id']]:
                 self.requests[json_data['req_id']].append(json_data['data'])
             else:
                 self.requests[json_data['req_id']]=[json_data['data']]
+            conn.close()
 
-        elif json_data['op']=='receive':
+        #ეს ბრძანება მოდის როცა კლიენტი გვეკითხება თუ ვებ respon-სის რამდენი ფრაგმენტს ელოდოს ჩვენგან
+        #თუ ამ ბრძანებას მივიღებთ,ეს ნიშნავს რომ კლიენტმა უკვე სრულად გამოგვიგზავნა ვებ request-ი და ეხლა web რესპონსს ელოდება
+        #ამავე კოდში უნდა მოხდეს ვებ რექუესთის რეალურ სერვერზე გაგზავნა და ვებ რესპონსის მიღება
+        #ვებ რესპონსის მიღების შემდეგ ცვენ ის უნდა დავყოთ ფრაგმენტებად 4000 ბაიტის ზომის და ვებ რექუესტების ლისტში უნდა შევყაროთ
+
+        elif json_data['op']=='receive_fr_count':
 
             fragment_list=self.requests['req_id']
+            conn.settimeout(settings.socket_timeout)
+            conn.sendall(str(len(fragment_list)).encode())
+            conn.settimeout(None)
+            conn.close()
 
-            for fragment in fragment_list
+
+
+
+
+        #თუ ეს ბრძანება მივიღეთ ესეიგი კლიენტი ითხოვს ვებ რესპონსის კონკრეტულ ფრაგმენტს ჩვენგან
+        elif json_data['op']=='receive_fr_data':
+
+            fragment_list=self.requests['req_id']
+            ragment_id=self.requests['fr_index']
+
+
+            conn.settimeout(settings.socket_timeout)
+
+
+
+            conn.settimeout(None)
+            conn.close()
+
+
+
+
+
+
+
+
 
 
 
