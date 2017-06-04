05dda2aaaf1d80a13cadb74756ff928bc31c301b
Administrator <kevlishvilimisha@gmail.com>
2017-06-04 10:50:40 +0200

    proxy receiver server

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socket
 import sys
 import io
 import threading
-
+import settings
 import time
 
 
@@ -27,21 +27,29 @@ class server_manager():
 
     def handle(self,conn,addr):
 
-
+        conn.settimeout(settings.socket_timeout)
         json_data=json.loads(conn.recv(4000).decode(),encoding='utf-8')
+        conn.settimeout(None)
 
 
 
         if json_data['op']=='send':
 
+            conn.settimeout(settings.socket_timeout)
             conn.sendall('ack')
+            conn.settimeout(None)
 
 
-            if self.requests[json_data['request_id']]:
-                self.requests[json_data['request_id']].append(json_data['data'])
+            if self.requests[json_data['req_id']]:
+                self.requests[json_data['req_id']].append(json_data['data'])
             else:
-                self.requests[json_data['request_id']]=[json_data['data']]
+                self.requests[json_data['req_id']]=[json_data['data']]
 
+        elif json_data['op']=='receive':
+
+            fragment_list=self.requests['req_id']
+
+            for fragment in fragment_list
 
 
 
---
+++ settings.py
@@ -0,0 +1,4 @@
+socket_timeout=5#წამი
+remote_server_ip='37.187.55.53'
+remote_server_port=1327
+max_resend_try=10
