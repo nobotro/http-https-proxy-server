2f9f1d88fff24e3c6f374b83c9461df21210e3f8
misha <kevlishvilimisha@yahoo.com>
2017-09-09 10:14:40 +0400

    pi

--- main.py
+++ main.py
@@ -336,18 +336,9 @@ class server_manager():
         
         
         elif json_data['op'] == 'https_receive_fr_count':
-            try:
-                request = self.requests[json_data['request_id']]['request']
-                if request == 'already_received':
-                    return
-                else:
-                    self.requests[json_data['request_id']]['request'] = 'already_received'
             
-            except Exception as e:
-                conn.sendto('0'.encode(), addr)
-                print('((((((((((((((((((=erori da rame ' + str(json_data))
-                logging.exception('message')
-                return
+            request = self.requests[json_data['request_id']]['request']
+            
             
             if json_data['request_id'] in self.https_sesions.keys():
                 sesion = self.https_sesions[json_data['request_id']]
