6f6e6e77e7c2bb3ebad9eefe9212aa8c8c8b11e2
misha <kevlishvilimisha@yahoo.com>
2017-09-09 20:32:23 +0400

    pi

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
-global_timeout=0.1#წამი
-remote_server_ip='188.226.160.247'
+
+
+global_timeout=0.8#წამი
+remote_server_ip='46.101.207.138'
 remote_server_port=1327
 max_resend_try=5
-max_fragment_size=65507
+max_fragment_size=40000
+
+clean_time=0.7
 
