b785f2173885538cb6cb99a6527890cb438dd049
misha <kevlishvilimisha@yahoo.com>
2017-09-09 09:18:08 +0400

    pi

--- main.py
+++ main.py
@@ -336,20 +336,6 @@ class server_manager():
         
         
         elif json_data['op'] == 'https_receive_fr_count':
-            try:
-                request = self.requests[json_data['request_id']]['request']
-                if request == 'already_received':
-                    fragment_list = len(self.requests[json_data['request_id']]['responce'])
-                    conn.sendto(str(fragment_list).encode(), addr)
-                    return
-                else:
-                    self.requests[json_data['request_id']]['request'] = 'already_received'
-            
-            except Exception as e:
-                
-                print('((((((((((((((((((=erori da rame ' + str(json_data))
-                logging.exception('message')
-                return
             
             if json_data['request_id'] in self.https_sesions.keys():
                 sesion = self.https_sesions[json_data['request_id']]
